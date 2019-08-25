3ac58966d52c263e1aec0c3c1bd7f6a66b37a0cc
Hemanthkumarg1987 <54210775+Hemanthkumarg1987@users.noreply.github.com>
2019-08-25 15:12:55 +0530

    Update Typecasting.py

--- Typecasting.py
+++ Typecasting.py
@@ -3,10 +3,24 @@
 x=1
 y=type(x)
 print(y)
-'''
+
 
 def demo_func(x,y=5):
   z=x+y
   return z
 
 print(demo_func(4))
+'''
+import json
+
+ec2 = '{ "region":"us-east1", "count":3, "type":"t2.micro"}'
+
+data = json.loads(ec2)
+
+print(data["region"])
+
+data["region"] = "eu-west2"
+
+ec2 = json.dumps(data)
+
+print(ec2)
