889174b97a3d9f6fbc1424ec77a0684c9e0d07fc
Hemanthkumarg1987 <54210775+Hemanthkumarg1987@users.noreply.github.com>
2019-08-26 11:33:44 +0530

    Rename Create_instance.py to Create_inst.py

---
+++ Create_inst.py
@@ -0,0 +1,12 @@
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+#define your instance
+instances = ec2.create_instances(
+     ImageId='ami-0d8f6eb4f641ef691',
+     MinCount=1,
+     MaxCount=2,
+     InstanceType='t2.micro',
+     KeyName='python-key'
+ )
