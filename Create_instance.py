32a2313527c64d2358e26ee848a3a40eda04bf57
Hemanthkumarg1987 <54210775+Hemanthkumarg1987@users.noreply.github.com>
2019-08-26 11:59:10 +0530

    Update Create_instance.py

--- Create_instance.py
+++ Create_instance.py
@@ -8,7 +8,6 @@ instances = ec2.create_instances(
      ImageId='ami-0d8f6eb4f641ef691',
      MinCount=1,
      MaxCount=2,
-     InstanceType='t2.micro',
-     KeyName='python-key'
+     InstanceType='t2.micro'
      
  )
