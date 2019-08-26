bcd9f9040bf65edd63b4c302e68ba61e4a0e3b1e
Hemanthkumarg1987 <54210775+Hemanthkumarg1987@users.noreply.github.com>
2019-08-26 11:58:14 +0530

    Update Create_instance.py

--- Create_instance.py
+++ Create_instance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import boto3
 
-ec2 = boto3.resource('ec2', region_name='us-east-2')
+ec2 = boto3.resource('ec2', region_name='us-east-2', aws_access_key_id='AKIAQ4DXDKQASL5ULNMC', aws_secret_access_key='JJcCtLyn3c0vpzaM8tKgA2zlLhgRR6Y17QMyDNPT')
 
 #define your instance
 instances = ec2.create_instances(
