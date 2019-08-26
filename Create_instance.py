3c845b73b8cfed531f02a51eaae07a71b7edf294
Hemanthkumarg1987 <54210775+Hemanthkumarg1987@users.noreply.github.com>
2019-08-26 11:35:05 +0530

    Update Create_instance.py

--- Create_instance.py
+++ Create_instance.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python
 import boto3
 
 ec2 = boto3.resource('ec2')
 
 #define your instance
 instances = ec2.create_instances(
-     ImageId='ami-0009a33f033d8b7b6',
+     ImageId='ami-0d8f6eb4f641ef691',
      MinCount=1,
      MaxCount=2,
      InstanceType='t2.micro',
